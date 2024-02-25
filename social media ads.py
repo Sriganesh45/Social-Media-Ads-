7e26c2cd9fbe006b6e6c65f85fd2be658dbeff22
Sriganesh45 <161178039+Sriganesh45@users.noreply.github.com>
2024-02-25 07:56:10 +0530

    Add files via upload

---
+++ social media ads.py
@@ -0,0 +1,62 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from matplotlib.colors import ListedColormap
+dataset = pd.read_csv('/content/Social_Network_Ads (1).csv')
+print(dataset.head())
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+print(X[:3, :])
+print('-'*15)
+print(y[:3])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+print(X_train[:3])
+print('-'*15)
+print(y_train[:3])
+print('-'*15)
+print(X_test[:3])
+print('-'*15)
+print(y_test[:3])
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+classifier = LogisticRegression(random_state = 0, solver='lbfgs' )
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+print(X_test[:10])
+print('-'*15)
+print(y_pred[:10])
+print(y_pred[:20])
+print(y_test[:20])
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+X_set, y_set = X_train, y_trainX1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),alpha = 0.6, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+X_set, y_set = X_test, y_testX1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),alpha = 0.6, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
